18d8899acd61399179910300fe5b6117fe9233ad
DanielDAW2 <rt.daniel.1994@gmail.com>
2019-11-25 19:17:10 +0100

    Add API to get Metode Pag

---
+++ ZintexIntranet/src/Controller/Api/TClientsController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller\Api;
+
+use App\Entity\TClients;
+use App\Repository\TClientsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/t/clients")
+ */
+class TClientsController extends AbstractController {
+
+    /**
+     * @Route("/get_metode_pag", name="t_clients_get_metode_pag")
+     */
+    public function getMetodePag(TClientsRepository $repo, Request $req) : JsonResponse {
+        
+        $client = $repo->find($req->get("client"));
+
+        
+
+        return new JsonResponse(["metode"=>$client->getMetPag()->getIdMetode(),"metodeDesc"=>$client->getMetPag()->getDescripcio()],200);
+
+    }
+}
+
+
+?>
