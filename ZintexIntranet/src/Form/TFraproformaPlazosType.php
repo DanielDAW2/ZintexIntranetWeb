231f1014e4009d4a0a11b6133e44c2ba8da94b1e
DanielDAW2 <rt.daniel.1994@gmail.com>
2019-11-25 18:28:09 +0100

    Update TFraproformaPlazosType.php

--- ZintexIntranet/src/Form/TFraproformaPlazosType.php
+++ ZintexIntranet/src/Form/TFraproformaPlazosType.php
@@ -14,7 +14,6 @@ class TFraproformaPlazosType extends AbstractType
         $builder
             ->add('descripprodFraprofPlazo')
             ->add('baixa')
-            ->add('codprodFraprofPlazo')
         ;
     }
 
